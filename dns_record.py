4ef9d4eec70bf0ef36860edbb61fbd4c863fde11
Tim Pietz <t.pietz97@gmail.com>
2016-09-29 21:40:03 +0200

    Add methods to generate value based on format

--- dns_record.py
+++ dns_record.py
@@ -1,3 +1,6 @@
+import re
+
+
 class DnsRecord:
     def __init__(self, bearer, domain, subdomain, dns_type, data_format):
         self.bearer = bearer
@@ -6,3 +9,24 @@ class DnsRecord:
         self.dns_type = dns_type
         self.data_format = data_format
         self.current_value = None
+
+    @staticmethod
+    def string_replace(string: str, ipv4: str, ipv6: str):
+        replacements = [('%v4', ipv4),
+                        ('%v6', ipv6),
+                        ('%%', '%')]
+
+        result = ''
+
+        while len(string) > 0:
+            if not string[0] == '%':
+                result += string[0]
+                string = string[1:]
+
+            else:
+                for key, value in replacements:
+                    if string.startswith(key):
+                        result += value
+                        string = string[len(key):]
+
+        return result
