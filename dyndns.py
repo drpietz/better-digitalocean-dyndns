b90fc788773b4ab06ce856f9c9a431af2247b0ac
Tim Pietz <t.pietz97@gmail.com>
2016-09-30 20:48:28 +0200

    Create main script which reads configuration and matches records to those fetched by api

---
+++ dyndns.py
@@ -0,0 +1,50 @@
+import json
+import socket
+from dns_record import DnsRecordFactory
+import api
+
+
+def main():
+    hostname = socket.gethostname()
+    records = get_records_from_config(hostname)
+    fetch_current_data(records)
+
+
+def fetch_current_data(records):
+    domain_records = {}
+
+    for record in records:
+        if record.domain not in domain_records:
+            domain_records[record.domain] = api.get_domain_records(record.bearer, record.domain)['domain_records']
+
+        current_domain_records = domain_records[record.domain]
+
+        for domain_record in current_domain_records:
+            if record.matches_record(domain_record):
+                record.set_base_record(domain_record)
+
+                current_domain_records.remove(domain_record)
+                break
+
+
+def get_records_from_config(hostname):
+    factory = DnsRecordFactory()
+
+    with open('config.json') as json_data:
+        config = json.load(json_data)
+
+    for bearer in config['bearers']:
+        factory.set_bearer(bearer['key'])
+
+        for domain in bearer['domains']:
+            factory.set_domain(domain['name'])
+
+            for record in domain['dyn_records']:
+                if 'device' not in record or record['device'] == hostname:
+                    factory.create_records(record['name'], record.get('type'), record.get('data'))
+
+    return factory.get_all_records()
+
+
+if __name__ == '__main__':
+    main()
