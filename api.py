e21ed6c75e091572bd5d2fb1844a2eeb83bdabd0
Tim Pietz <t.pietz97@gmail.com>
2016-10-05 17:30:54 +0200

    Check response after making api calls. Closes #2

--- api.py
+++ api.py
@@ -8,21 +8,36 @@ def get_domain_records(bearer, domain):
     url = '/v2/domains/{domain}/records'.format(domain=domain)
 
     response = call('GET', url, bearer)
-    return response.json()
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise ValueError('Unexpected response ({code}) while getting domain records'.format(code=response.status_code),
+                         domain, get_api_response_error_text(response))
 
 
 def create_domain_record(bearer, domain, data):
     url = '/v2/domains/{domain}/records'.format(domain=domain)
 
     response = call('POST', url, bearer, data)
-    return response.json()
+
+    if response.status_code == 201:
+        return response.json()
+    else:
+        raise ValueError('Unexpected response ({code}) while creating domain record'.format(code=response.status_code),
+                         domain, get_api_response_error_text(response))
 
 
 def update_domain_record(bearer, domain, record_id, data):
     url = '/v2/domains/{domain}/records/{id}'.format(domain=domain, id=record_id)
 
     response = call('PUT', url, bearer, data)
-    return response.json()
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise ValueError('Unexpected response ({code}) while updating domain record'.format(code=response.status_code),
+                         domain, get_api_response_error_text(response))
 
 
 def call(method, url, bearer, data=None):
@@ -30,6 +45,18 @@ def call(method, url, bearer, data=None):
     auth_header = get_auth_header(bearer)
 
     return requests.request(method, absolute_url, headers=auth_header, data=data)
+
+
+def get_api_response_error_text(response):
+    try:
+        json = response.json()
+
+        if 'id' in json and 'message' in json:
+            return '{id}: {message}'.format(id=json['id'], message=json['message'])
+        else:
+            return response.text
+    except ValueError:
+        return response.text
 
 
 def get_auth_header(bearer):
